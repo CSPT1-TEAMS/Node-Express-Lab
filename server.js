b58bdc1151fca423527d8bb81d86d9a504f6ea50
Alexander <alexander.botello@g.austincc.edu>
2018-05-15 21:09:51 -0500

    Add post, modify get

--- server.js
+++ server.js
@@ -3,8 +3,40 @@ const db = require('./data/db.js');
 
 const server = express()
 
-server.get('/api/posts')
-server.get('/api/posts/:id')
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then(posts => {
+      res.json({posts})
+    })
+    .catch(err => {
+      res.status(500).json({ error: "There was an error while saving the post to the database" })
+    })
+})
+server.get('/api/posts/:id', (req, res) => {
+  const postId = req.params.id;
+  db.findById(postId)
+    .then(post => {
+      res.json({post})
+    })
+    .catch(error => {
+      res.status(404).json({ message: "The post with the specified ID does not exist." })
+    })
+})
+
+server.post('/api/posts', (req, res) => {
+  const post = req.body
+  if (!post.title && !post.content) {
+    res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+  }
+  db.insert(post)
+    .then(posts => {
+      res.status(201).json({posts})
+    })
+    .catch(err => {
+      res.status(500).json({ error: "There was an error while saving the post to the database" })
+    })
+
+})
 
 server.listen(5005, () => {
   console.log('Server is running on port 5005')
