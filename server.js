e5be2ae6fab60c52cf593f46434975a2c8762373
JohnCoronel <john.m.coronel@gmail.com>
2018-05-15 20:26:04 -0700

    finished CRUD

--- server.js
+++ server.js
@@ -8,15 +8,35 @@ const server = express();
 server.use(bodyParser.json())
 server.use(express.json());
 
-server.post('/api/posts', (req,res) => {
-    console.log('req.body is ',req.body)
-    const post = req.body; 
-    console.log(post)
-    
-    db.insert(post).then( (post) => {
-        res.status(200).json(post)
+
+server.get('/api/posts',(req,res) => {
+    db.find().then((posts) => {
+        res.status(200).json(posts)
     })
 })
+
+server.get('/api/posts/:id',(req,res) => {
+    db.findById(req.params.id).then( (post)=>
+        {res.status(200).json(post)}
+    )
+})
+
+
+
+
+
+server.post('/api/posts', (req,res) => {
+    console.log('req.body is ',req.body)
+    const{title, contents} = req.body;
+    const post = req.body; 
+    console.log(post)
+    db.insert({title,contents}).then( (post) => {
+             console.log(post)
+              res.status(200).json({...post,title,contents})
+    })
+})
+
+
 
 server.delete('/api/posts/:id', (req, res) => {
     const { id } = req.params
@@ -32,6 +52,19 @@ server.delete('/api/posts/:id', (req, res) => {
             })
         }
     })
+})
+
+server.put('/api/posts/:id', (req,res) => {
+    const {id} = req.params
+     const {title,contents} = req.body
+    if (!req.body.title || !req.body.contents){
+        res.status(400).json({message:"Please provide title and contents for the post"})
+    }
+    db.update(id,req.body).then((post)=>{
+        console.log(post)
+        res.status(200).json({id:post,title,contents})
+    })
+
 })
 
 server.listen(5000,() => {
