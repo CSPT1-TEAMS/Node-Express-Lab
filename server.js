77bdecc857697f4db98b0811a339acaedb0049f2
JohnCoronel <john.m.coronel@gmail.com>
2018-05-15 19:25:43 -0700

    post done

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
 // import your node modules
-
+const express = require('express')
+const bodyParser = require('body-parser')
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+server.use(bodyParser.json())
+server.use(express.json());
+
+server.post('/api/posts', (req,res) => {
+    console.log('req.body is ',req.body)
+    const post = req.body; 
+    console.log(post)
+    
+    db.insert(post).then( (post) => {
+        res.status(200).json(post)
+    })
+})
+
+server.listen(5000,() => {
+    console.log('Listening on port 5000');
+})
+ 
