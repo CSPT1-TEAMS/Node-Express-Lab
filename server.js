d0af62242c08c7cbad85325bd7c2ef8e91017e26
Andrew Jarrett <ahrjarrett@gmail.com>
2018-05-15 21:08:13 -0500

    create branch; start working in server.js

--- server.js
+++ server.js
@@ -1,5 +1,27 @@
-// import your node modules
+const express = require('express')
+const bodyParser = require('body-parser')
+const db = require('./data/db.js')
+const app = express()
+const PORT = 4337
 
-const db = require('./data/db.js');
+app.use(bodyParser({ extended: true }))
+app.use(bodyParser.json())
 
-// add your server code starting here
+app.post('/api/posts', (req, res) => {
+  const { title, contents } = req.body
+
+  if (!title || !contents)
+    return res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+
+  console.log('in post route')
+  console.log(title)
+  console.log(contents)
+
+  return res.status(200)
+})
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+
+
+
+
