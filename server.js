c2a8a757233a8243f6c636c21c1589e91f1c2236
JPEmery <clearkeyemery@gmail.com>
2018-05-15 20:50:15 -0500

    JP - bhavik - jesse: updated npm

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ const express = require('express';)
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+server.listen(3000, () => {
+    console.log('app is running on port 3000');
+})
+
