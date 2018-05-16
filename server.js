b3e33d5bfbc3106a678209f3edd11ade4347f0a3
Stephanie <sbowl001@fiu.edu>
2018-05-16 11:40:59 +0800

    working on delete

--- server.js
+++ server.js
@@ -42,6 +42,36 @@ server.post('/api/posts', (req, res) => {
     });
 });
 
+server.delete('/api/posts/:id', (req, res) => {
+  const id = req.params.id;
+  db.findById(id)
+     .then(post => {
+      if (post.length > 0) {
+        db.remove(id)
+          .then( response => {
+            res.status(200).json(response)
+          }) 
+      }
+      res.status(500).json({ error: "The post could not be removed" });
+     })
+     .catch(error => {
+       res.status(404).json({
+           message: "The post with the specified ID does not exist."
+         });
+     });
+})
+
 server.listen(5005, () => {
   console.log('Server is running on port 5005')
 })
+
+// If the post with the specified id is not found:
+
+// return HTTP status code 404 (Not Found).
+// return the following JSON object: { message: "The post with the specified ID does not exist." }.
+
+// If there's an error in removing the post from the database:
+
+// cancel the request.
+// respond with HTTP status code 500.
+// return the following JSON object: { error: "The post could not be removed" }.
