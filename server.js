194dd203ca86ec02261497bea5014d9f4baad269
Victor Montoya <victoramontoya@gmail.com>
2018-05-15 19:30:38 -0700

    get & get :id works

--- server.js
+++ server.js
@@ -1,5 +1,41 @@
-// import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
+const server = express();
 
-// add your server code starting here
+
+
+server.listen(5000, () => {
+    console.log('=== APP running on port 5000 ===')
+})
+
+server.get('/api/posts', function (req, res) { 
+    db.find()
+    .then( posts => {
+        res.json({ posts })
+    })
+    .catch( error => {
+        res.status(500).json({ 
+            error: "The posts information could not be retrieved." 
+        })
+    }) 
+})
+
+server.get('/api/posts/:id', function (req, res) {
+    const postId = req.params.id;
+    db.findById(postId)
+        .then( post => {
+            res.json({ post })
+        })
+        .catch(error => {
+            if (res.statusCode === 404) {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist."
+                 })
+            } else {
+                res.status(500).json({
+                    error: "The posts information could not be retrieved."
+                })
+            }
+
+        })
+})
