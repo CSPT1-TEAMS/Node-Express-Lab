88d2e915c9d32dd1119016cab007f2dbb03176ef
Victor Montoya <victoramontoya@gmail.com>
2018-05-16 18:42:23 -0700

    post works

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const db = require('./data/db.js');
 const server = express();
 
 
-
 server.listen(5000, () => {
     console.log('=== APP running on port 5000 ===')
 })
+
+server.use(express.json());
+
 
 server.get('/api/posts', function (req, res) { 
     db.find()
@@ -37,5 +39,24 @@ server.get('/api/posts/:id', function (req, res) {
                 })
             }
 
+        })
+})
+
+server.post('/api/posts', (req, res) => {
+    const newPost = req.body;
+    db.insert(newPost)
+        .then(post => {
+            res.status(201).json({ post });
+        })
+        .catch(err => {
+            if (res.statusCode === 400) {
+                res.status(400).json({
+                    errorMessage: "Please provide title and contents for the post."
+                })
+            } else {
+                res.status(500).json({
+                    error: "There was an error while saving the post to the database"
+                })
+            }
         })
 })
